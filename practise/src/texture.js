abcbed517d0529ed4022c29c5795c0eafb33f2d3
pratyush <pk17111039@gmail.com>
2026-05-25 22:36:44 +0530

    textures repetition

--- practise/src/texture.js
+++ practise/src/texture.js
@@ -11,6 +11,7 @@ const camera = new THREE.PerspectiveCamera(
     100
 );
 const controls = new OrbitControls(camera, canvas);
+const texureLoader = new THREE.TextureLoader();
 
 scene.add(camera);
 camera.position.z = 10;
@@ -19,9 +20,21 @@ const cubeGeometry = new THREE.BoxGeometry(1,1,1);
 const cyclinderGeometry = new THREE.CylinderGeometry(1,1,1,32);
 const sphereGeometry = new THREE.SphereGeometry(1,32,32);
 const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5,0.15,100,16);
-const planeGeomtry = new THREE.PlaneGeometry(1,1);
+const planeGeomtry = new THREE.PlaneGeometry(3,3);
 
 const material = new THREE.MeshBasicMaterial();
+const textureTest = texureLoader.load('textures2/1st.jpg');
+textureTest.repeat.set(4,4);
+// textureTest.wrapS = THREE.RepeatWrapping;
+// textureTest.wrapT = THREE.RepeatWrapping;
+
+textureTest.wrapS = THREE.MirroredRepeatWrapping;
+textureTest.wrapT = THREE.MirroredRepeatWrapping;
+
+// textureTest.wrapS = THREE.ClampToEdgeWrapping;
+// textureTest.wrapT = THREE.ClampToEdgeWrapping;
+
+material.map = textureTest;
 const group = new THREE.Group();
 
 const cubeMesh = new THREE.Mesh();
@@ -41,7 +54,8 @@ torusKnotMesh.position.y = -3;
 const planeMesh = new THREE.Mesh(planeGeomtry, material);
 material.side = THREE.DoubleSide;
 
-group.add(cubeMesh, spherMesh, torusKnotMesh, cyclinderMesh, planeMesh);
+// group.add(cubeMesh, spherMesh, torusKnotMesh, cyclinderMesh, planeMesh);
+group.add(planeMesh);
 scene.add(group);
 
 const renderer = new THREE.WebGLRenderer({
@@ -50,11 +64,11 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-controls.autoRotate = true;
 controls.enableDamping = true;
 
 function animation(){
     controls.update();
+    // group.rotation.x +=0.01;
     requestAnimationFrame(animation);
     renderer.render(scene, camera);
 }
